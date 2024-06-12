479afd067c3d86f78332c841b0d32d6ec4aece91
naystry <nisrinaraniah03@gmail.com>
2024-06-13 02:01:41 +0700

    update server

--- src/services/loadModel.js
+++ src/services/loadModel.js
@@ -2,7 +2,7 @@ const tf = require("@tensorflow/tfjs-node");
 
 const loadModel = async () => {
   //return tf.loadLayersModel("file://InceptionV3/model.json");
-return tf.loadLayersModel(process.ENV.MODEL_URL);
+return tf.loadLayersModel('https://storage.googleapis.com/modelinceptionv3/InceptionV3/model.json');
 }
 
 module.exports = loadModel;
