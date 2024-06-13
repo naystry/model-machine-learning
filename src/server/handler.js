f2ba881af7ae25844971ece7d9248ad3ed6defcc
naystry <nisrinaraniah03@gmail.com>
2024-06-13 14:27:19 +0700

    update server

--- src/server/handler.js
+++ src/server/handler.js
@@ -3,6 +3,21 @@ const crypto = require("crypto");
 
 //const { storeData, getDatas } = require("../services/storeData");
 
+const createUnixSocketPool = async config => {
+  return mysql.createPool({
+    user: process.env.DB_USER, // e.g. 'my-db-user'
+    password: process.env.DB_PASS, // e.g. 'my-db-password'
+    database: process.env.DB_NAME, // e.g. 'my-database'
+    socketPath: process.env.INSTANCE_UNIX_SOCKET, // e.g. '/cloudsql/project:region:instance'
+  });
+};
+
+let pool;
+(async () => {
+    pool = await createUnixSocketPool();
+})();
+
+
 const color_palette = {
   light: ["#ffffff", "#ffc8dd", "#ffafcc", "#bde0fe", "#a2d2ff"],
   dark: ["#03045e", "#832161", "#363062", "#751628", "#bc455a"],
--- src/services/loadModel.js
+++ src/services/loadModel.js
@@ -2,7 +2,7 @@ const tf = require("@tensorflow/tfjs-node");
 
 const loadModel = async () => {
   //return tf.loadLayersModel("file://InceptionV3/model.json");
-return tf.loadLayersModel('https://storage.googleapis.com/modelinceptionv3/InceptionV3/model.json');
+return tf.loadLayersModel('https://storage.googleapis.com/skintone-ml/model.json');
 }
 
 module.exports = loadModel;
